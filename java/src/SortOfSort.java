c1442308c37bf890c50214d3e391ea98d1232e6c
Raisa-NN <rnnkweteyim@miners.utep.edu>
2020-03-02 14:49:16 -0700

    Figuring out algorithm

---
+++ java/src/SortOfSort.java
@@ -0,0 +1,13 @@
+public class SortOfSort {
+    //Gain inspiration from selection sort algorithm
+    //Find Max
+    //aFTER every findMax, decrement length of array
+    //Swap to end
+    //Find 2nd Max
+    //Swap tp end
+    //Find 3rd Max,
+    //Add to beginning
+    //Find 4th Max
+    //Add to beeginning
+    //Loop entire processes
+}
