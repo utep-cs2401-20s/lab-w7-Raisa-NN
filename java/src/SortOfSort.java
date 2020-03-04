01d50833f405fb2056e1e7e09014f79cf9476da5
Raisa-NN <rnnkweteyim@miners.utep.edu>
2020-03-04 14:49:29 -0700

    Finished findMax method and began SortOfSort

--- java/src/SortOfSort.java
+++ java/src/SortOfSort.java
@@ -1,13 +1,54 @@
 public class SortOfSort {
-    //Gain inspiration from selection sort algorithm
-    //Find Max
-    //aFTER every findMax, decrement length of array
-    //Swap to end
-    //Find 2nd Max
-    //Swap tp end
-    //Find 3rd Max,
-    //Add to beginning
-    //Find 4th Max
-    //Add to beeginning
-    //Loop entire processes
+    public void sort(int array[])
+    { int count = 0;
+    int begin = 0;
+    int end = array.length;
+    int max;
+        // One by one move boundary of unsorted subarray
+    while (begin < end) {
+
+        // Find the maximum element in unsorted array
+        max = findMax(array);
+
+        // Swap the found maximum element with the last element
+        int temp = max;
+        max = array[array.length - 1 - count];
+        array[array.length - 1 - count] = temp;
+        count++;
+        end--;
+    }
+
+            //After sorting the last 2 elements correctly (put to end), the next largest numbers are swapped to the beginning instead
+            // Swap the found maximum element with the first element
+//            if (count == 3)
+//                temp = array[max];
+//                array[max] = array[i+i];
+//                array[i+i] = temp;
+//                count = 0;
+
+    public int findMax(int[] array) {
+        int max = array[0];
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] > max) {
+                max = array[i];
+            }
+         return max;
+    }
+    // Prints the array
+    public void printArray(int array[])
+    {
+        for (int i=0; i<array.length; ++i)
+            System.out.print(array[i]+" ");
+        System.out.println();
+    }
+
+    // Driver code to test above
+    public static void main(String args[])
+    {
+        SortOfSort ob = new SortOfSort();
+        int array[] = {2,7,1,3,0,9,6,5};
+        ob.sort(array);
+        System.out.println("Sorted array");
+        ob.printArray(array);
+    }
 }
